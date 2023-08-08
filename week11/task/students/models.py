1dcae66c7da85f8a55f00e8132492c6d92cfd1ae
nursultan <nursultankalilov1@gmail.com>
2023-08-08 14:33:20 +0600

    task

---
+++ week11/task/students/models.py
@@ -0,0 +1,15 @@
+from django.db import models
+
+
+class Student(models.Model):
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    email =  models.EmailField()
+    age = models.IntegerField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+class Course(models.Model):
+    name = models.CharField(max_length= 100)
+    description = models.TextField(blank=True, verbose_name='Описание')
+    students =  models.ManyToManyField(Student, verbose_name= 'Курс')
